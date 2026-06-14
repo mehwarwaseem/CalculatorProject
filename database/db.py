b957df547d41e9627ff79b2d47095b5c89fe7f5f
user.email <ziar4556@gmail.com>
2026-06-14 11:22:59 +0500

    Added database history functions

--- database/db.py
+++ database/db.py
@@ -8,10 +8,11 @@ class Database:
         self.cursor.execute("""
         CREATE TABLE IF NOT EXISTS history (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            expression TEXT,
-            result TEXT
+            expression TEXT NOT NULL,
+            result TEXT NOT NULL
         )
         """)
+
         self.conn.commit()
 
     def save(self, expression, result):
@@ -20,3 +21,18 @@ class Database:
             (expression, str(result))
         )
         self.conn.commit()
+
+    def get_history(self):
+        self.cursor.execute("""
+            SELECT expression, result
+            FROM history
+            ORDER BY id DESC
+        """)
+        return self.cursor.fetchall()
+
+    def clear_history(self):
+        self.cursor.execute("DELETE FROM history")
+        self.conn.commit()
+
+    def close(self):
+        self.conn.close()
